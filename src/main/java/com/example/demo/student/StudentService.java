d58484775f9eb0db5627caf760134315c1068294
Viktor Stepanenko <vstepanenko@gmail.com>
2024-03-18 17:07:07 +0100

    Add StudentRepository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,20 +1,21 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(
-            1L,
-            "Viktor",
-            "vstepanenko@gmail.com",
-            LocalDate.of(1983, Month.JANUARY, 15),
-            41
-            ));
+        return studentRepository.findAll();
     }
 }
