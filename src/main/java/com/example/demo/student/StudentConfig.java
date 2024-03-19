a5342404f813aef6c36e42e0e85b4629081e586e
Viktor Stepanenko <vstepanenko@gmail.com>
2024-03-19 16:53:41 +0100

    Fix age

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -14,8 +14,8 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
-                        Student mariam = new Student( "Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 24);
-                        Student alex = new Student( "Alex", "alex@gmail.com", LocalDate.of(2004, Month.JANUARY, 5), 20);
+                        Student mariam = new Student( "Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5));
+                        Student alex = new Student( "Alex", "alex@gmail.com", LocalDate.of(2004, Month.JANUARY, 5));
 
             repository.saveAll(List.of(mariam, alex));
         };
